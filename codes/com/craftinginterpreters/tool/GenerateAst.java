da6c1993d0f4285bcb057bfa33231253f163d122
okanoshion <cish22083@g.nihon-u.ac.jp>
2024-12-17 11:19:54 +0900

    modify GenerateAst.java for visitor

--- codes/com/craftinginterpreters/tool/GenerateAst.java
+++ codes/com/craftinginterpreters/tool/GenerateAst.java
@@ -19,6 +19,7 @@ public class GenerateAst {
         "Unary    : Token operator, Expr right"
       ));
   }
+
   private static void defineAst(
       String outputDir, String baseName, List<String> types)
       throws IOException {
@@ -31,9 +32,39 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    // Add the abstract accept method here
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
+    defineVisitor(writer, baseName, types);
+    defineTypes(writer, baseName, types);
+
     writer.println("}");
     writer.close();
   }
+
+  private static void defineVisitor(
+      PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" +
+          typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
+  }
+
+  private static void defineTypes(
+      PrintWriter writer, String baseName, List<String> types) {
+    for (String type : types) {
+      String className = type.split(":")[0].trim();
+      String fieldList = type.split(":")[1].trim();
+      defineType(writer, baseName, className, fieldList);
+    }
+  }
+
   private static void defineType(
       PrintWriter writer, String baseName,
       String className, String fieldList) {
@@ -50,6 +81,13 @@ public class GenerateAst {
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" +
+        className + baseName + "(this);");
     writer.println("    }");
 
     // Fields.
